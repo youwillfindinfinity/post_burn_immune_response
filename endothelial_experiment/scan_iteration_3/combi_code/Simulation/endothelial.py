61fb3de011393baed041446f617b85961d011571
youwillfindinfinity <rbumbuc@gmail.com>
2023-08-08 19:55:17 +0200

    Update

---
+++ endothelial_experiment/scan_iteration_3/combi_code/Simulation/endothelial.py
@@ -0,0 +1,19 @@
+
+from cc3d import CompuCellSetup
+from variablevals import relaxationmcs      
+
+from endothelialSteppables import endothelialSteppable
+
+CompuCellSetup.register_steppable(steppable=endothelialSteppable(frequency=int(relaxationmcs)))
+
+
+
+        
+from endothelialSteppables import celldivisionSteppable
+CompuCellSetup.register_steppable(steppable=celldivisionSteppable(frequency=int(relaxationmcs)))
+
+
+
+
+
+CompuCellSetup.run()
