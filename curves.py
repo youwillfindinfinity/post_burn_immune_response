88fbd52f268bc0cef12759848aa509967d175f94
youwillfindinfinity <rbumbuc@gmail.com>
2023-08-17 11:22:16 +0200

    Update

--- curves.py
+++ curves.py
@@ -2,13 +2,13 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Sigmoid function
 def sigmoid(x, a, b):
     return 1 / (1 + np.exp(-a * (x - b)))
 
-# Michaelis-Menten curve
 def michaelis_menten(x, km, vmax):
-    return (vmax * x) / (km + x)
+    condition = (vmax * x) == (km + x)
+    mm = np.where(condition, (vmax * (x + 1)) / (km + x), (vmax * x) / (km + x))
+    return np.abs(mm)
 
 # Parameter values for subplots
 sigmoid_params = [(1, 4), (2, 4), (1, 5), (2, 5)]
@@ -33,22 +33,24 @@ for km, vmax in mm_params:
     label = f'km = {km}, vmax = {vmax}'
     sns.lineplot(x=x, y=y, label=label, ax=axs[1])
 
-    # Set custom x-axis tick labels
-    axs[1].set_xticks(np.arange(0, 110, 10))
-    axs[1].set_xticklabels(np.arange(0, 110, 10))
+    # Set custom x-axis tick labels for different intervals
+    x_ticks = np.concatenate((np.arange(0, 60, 30), np.arange(50, 115, 15)))
+    axs[1].set_xticks(x_ticks)
+    axs[1].set_xticklabels(x_ticks)
 
 # Set titles and legends
 axs[0].set_title('Sigmoid Function')
-axs[0].set_ylabel('Probability of activation')
+axs[0].set_ylabel('Probability of transition')
 axs[0].set_xlabel('Concentration of cytokines')
 axs[0].set_xlim(0, 10)
-axs[0].legend()
+# axs[0].legend().set_visible(False)
+
 
 axs[1].set_title('Michaelis-Menten Curve')
-axs[1].set_ylabel('m2deathspan')
-axs[1].set_xlabel('Time(h)')
-axs[1].set_xlim(95, 100)
-axs[1].legend()
+axs[1].set_ylabel('m2lifepan')
+axs[1].set_xlabel('Time (h)')
+axs[1].set_xlim(90, 101)
+# axs[1].legend().set_visible(False)
 
 # Add a big title at the top
 fig.suptitle('Comparison of Sigmoid and Michaelis-Menten Curves')
@@ -57,7 +59,7 @@ fig.suptitle('Comparison of Sigmoid and Michaelis-Menten Curves')
 plt.tight_layout()
 
 # Save the plot as a PNG
-plt.savefig('curves_plot.png', dpi=300)
+plt.savefig('curves_plot1.png', dpi=300)
 
 # Show the plot
 plt.show()
